cb39a4090af197ba545280af0026f45c3292ec25
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-23 10:59:04 +0900

    Update Maximum-Subarray-Sum.cs

--- Maximum-Subarray-Sum.cs
+++ Maximum-Subarray-Sum.cs
@@ -1,1 +1,32 @@
-CS
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'maximumSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER_ARRAY a
+     *  2. LONG_INTEGER m
+     */
+
+    public static long maximumSum(List<long> a, long m)
+    {
+
+    }
+
+}
