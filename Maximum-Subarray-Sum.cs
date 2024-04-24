19ec959474aa7dedfddc25621679b54c19003766
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-24 15:02:38 +0900

    Update Maximum-Subarray-Sum.cs

--- Maximum-Subarray-Sum.cs
+++ Maximum-Subarray-Sum.cs
@@ -30,3 +30,31 @@ class Result
     }
 
 }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            long m = Convert.ToInt64(firstMultipleInput[1]);
+
+            List<long> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt64(aTemp)).ToList();
+
+            long result = Result.maximumSum(a, m);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
